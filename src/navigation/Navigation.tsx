e973a0beb3d0d8a7914de667e98a812a0dfd3683
Daniel Briceno <daniel.b100726@gmail.com>
2023-05-13 23:19:46 -0400

    Clase 2: Configuración de React Navigation

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Account from "../screens/Account";
+import Favorite from "../screens/Favorite";
+import Pokedex from "../screens/Pokedex";
+
+const Tab = createBottomTabNavigator()
+
+export default function Navigation() {
+  return (
+    <Tab.Navigator>
+       <Tab.Screen name='Favorite' component={Favorite}/>
+       <Tab.Screen name='Pokedex' component={Pokedex}/>
+      <Tab.Screen name='Account' component={Account}/>
+    </Tab.Navigator>
+  );
+}
