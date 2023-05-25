371a478c3704767b3a797c1e67f43345748deea0
Daniel Briceno <daniel.b100726@gmail.com>
2023-05-24 21:55:00 -0400

    advances

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -5,14 +5,14 @@ import Account from "../screens/Account";
 import Favorite from "../screens/Favorite";
 import Pokedex from "../screens/Pokedex";
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
   return (
     <Tab.Navigator>
-       <Tab.Screen name='Favorite' component={Favorite}/>
-       <Tab.Screen name='Pokedex' component={Pokedex}/>
-      <Tab.Screen name='Account' component={Account}/>
+      <Tab.Screen name="Favorite" component={Favorite} />
+      <Tab.Screen name="Pokedex" component={Pokedex} />
+      <Tab.Screen name="Account" component={Account} />
     </Tab.Navigator>
   );
 }
